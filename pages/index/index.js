2cf57dcd90e6e4905b4d3cae887604d718a5d72d
bruce1106 <969597302@qq.com>
2018-09-01 12:35:01 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,139 @@
 //index.js
-//获取应用实例
-const app = getApp()
-
+var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    currentTab: 0,
+    qunLists: [
+      {
+        src: '',
+        content: '1213123'
+      },
+      {
+        src: '',
+        content: '1213123'
+      },
+      {
+        src: '',
+        content: '1213123'
+      },
+      {
+        src: '',
+        content: '1213123'
+      },
+      {
+        src: '',
+        content: '1213123'
+      },
+      {
+        src: '',
+        content: '1213123'
+      },
+      {
+        src: '',
+        content: '1213123'
+      },
+      {
+        src: '',
+        content: '1213123'
       }
+    ],
+    personalLists: [
+      {
+        src: '',
+        content: '3333'
+      },
+      {
+        src: '',
+        content: '42342'
+      },
+      {
+        src: '',
+        content: '22'
+      },
+      {
+        src: '',
+        content: 'dsdfasasdf'
+      },
+      {
+        src: '',
+        content: 'asdfasdf'
+      },
+      {
+        src: '',
+        content: 'asdfasdgg'
+      },
+      {
+        src: '',
+        content: 'gdss'
+      },
+      {
+        src: '',
+        content: '121sdasdfa3123'
+      },
+      {
+        src: '',
+        content: '121sdfasddasdfa3123'
+      }
+    ],
+    gzhLists: [
+      {
+        src: '',
+        content: '3333'
+      },
+      {
+        src: '',
+        content: '42342'
+      },
+      {
+        src: '',
+        content: '22'
+      },
+      {
+        src: '',
+        content: 'dsdfasasdf'
+      },
+      {
+        src: '',
+        content: 'asdfasdf'
+      },
+      {
+        src: '',
+        content: 'asdfasdgg'
+      },
+      {
+        src: '',
+        content: 'gdss'
+      },
+      {
+        src: '',
+        content: '121sdasdfa3123'
+      },
+      {
+        src: '',
+        content: '121sdfasddasdfa3123'
+      }
+    ]
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+
+  },
+  //滑动切换
+  swiperTab: function (e) {
+    var that = this;
+    that.setData({
+      currentTab: e.detail.current
+    });
+  },
+  //点击切换
+  clickTab: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+      that.setData({
+        currentTab: e.target.dataset.current
       })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
   }
 })
