bea7116a248dc4e414ec64d311aef26f524a01fc
Pan Liu <pan.liu@supernovacompanies.biz>
2018-09-03 08:58:48 +0800

    change xiaochengxu

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,145 @@
 //index.js
-//获取应用实例
-const app = getApp()
-
+var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    currentTab: 0,
+    qunLists: [
+      {
+        src: 'http://bpic.588ku.com/element_origin_min_pic/16/10/30/528aa13209e86d5d9839890967a6b9c1.jpg',
+        content: '1213123'
+      },
+      {
+        src: 'http://bpic.588ku.com/element_origin_min_pic/16/10/30/54fcef525fa8f6037d180f3c26f3be65.jpg',
+        content: '1213123'
+      },
+      {
+        src: 'http://bpic.588ku.com/element_origin_min_pic/16/10/30/62e3ca3a02dddb002eff00482078d194.jpg',
+        content: '1213123'
+      },
+      {
+        src: 'http://bpic.588ku.com/element_origin_min_pic/16/10/31/c7167fcfb4ebcd12621c05b0c852e98e.jpg',
+        content: '1213123'
       }
+    ],
+    personalLists: [
+      {
+        src: '',
+        content: '3333'
+      },
+      {
+        src: '',
+        content: '42342'
+      },
+      {
+        src: '',
+        content: '22'
+      },
+      {
+        src: '',
+        content: 'dsdfasasdf'
+      },
+      {
+        src: '',
+        content: 'asdfasdf'
+      },
+      {
+        src: '',
+        content: 'asdfasdgg'
+      },
+      {
+        src: '',
+        content: 'gdss'
+      },
+      {
+        src: '',
+        content: '121sdasdfa3123'
+      },
+      {
+        src: '',
+        content: '121sdfasddasdfa3123'
+      }
+    ],
+    gzhLists: [
+      {
+        src: '',
+        content: '3333'
+      },
+      {
+        src: '',
+        content: '42342'
+      },
+      {
+        src: '',
+        content: '22'
+      },
+      {
+        src: '',
+        content: 'dsdfasasdf'
+      },
+      {
+        src: '',
+        content: 'asdfasdf'
+      },
+      {
+        src: '',
+        content: 'asdfasdgg'
+      },
+      {
+        src: '',
+        content: 'gdss'
+      },
+      {
+        src: '',
+        content: '121sdasdfa3123'
+      },
+      {
+        src: '',
+        content: '121sdfasddasdfa3123'
+      }
+    ]
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+
+  },
+  //滑动切换
+  swiperTab: function (e) {
+    var that = this;
+    that.setData({
+      currentTab: e.detail.current
+    });
+  },
+  //点击切换
+  clickTab: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+      that.setData({
+        currentTab: e.target.dataset.current
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+
+  //预览图片
+  previewImages (e) {
+    let that = this;
+    let index = e.currentTarget.dataset.index;
+    let tArr = [];
+    if (this.data.currentTab === 0) {
+      tArr = this.data.qunLists;
+    } else if (this.data.currentTab === 1) {
+      tArr = this.data.personalLists;
+    } else {
+      tArr = this.data.gzhLists;
+    }
+    let imgArr = tArr.map(val => val.src)
+    wx:wx.previewImage({
+      current: imgArr[index],
+      urls: imgArr,
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
     })
   }
 })
